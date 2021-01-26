b981f4a51c63a6cc1b7fdde7fe8f0114998e0212
omali2@yahoo.de <omali2@yahoo.de>
2021-01-26 14:06:11 +0100

    update aws infra

--- modules/network/subnet/database/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "database_subnet_id" {
-  description = "The ID of the subnet"
-  value       = join("", aws_subnet.main[*].id)
-}
--- modules/network/subnet/public/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_subnet_id" {
-  description = "The ID of the subnet"
-  value       = join("", aws_subnet.main[*].id)
-}
